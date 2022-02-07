53d60c31dafe396a606b376a280577b1405ae017
DJFriar <tommy.craft@icloud.com>
2022-02-07 03:13:57 -0600

    updated contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,21 +5,22 @@ import PageBanner from '../components/Common/PageBanner'
 import Footer from "../components/App/Footer"
 import ContactInfo from '../components/Contact/ContactInfo'
 import ContactForm from '../components/Contact/ContactForm'
+
 const Contact = () => {
-    return (
-        <Layout>
-            <Navbar />
-            <PageBanner
-                pageTitle="Contact" 
-                homePageText="Home" 
-                homePageUrl="/" 
-                activePageText="Contact" 
-            />
-            <ContactInfo />
-            <ContactForm />
-            <Footer />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Navbar />
+      <PageBanner
+        pageTitle="Contact" 
+        homePageText="Home" 
+        homePageUrl="/" 
+        activePageText="Contact" 
+      />
+      <ContactInfo />
+      <ContactForm />
+      <Footer />
+    </Layout>
+  );
 }
 
 export default Contact
